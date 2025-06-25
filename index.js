a9aab5ec827b8d48b46c61527caf2c45bfb40e8e
Vkaisang41 <kaisang.vincent@student.moringaschool.com>
2025-06-25 23:35:48 +0300

    Implement submitData function and pass all tests

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const p = document.createElement("p");
+      p.textContent = data.id;
+      document.body.appendChild(p);
+    })
+    .catch((error) => {
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      document.body.appendChild(p);
+    });
+}
+
+// Leave this at the bottom of index.js so the test file can access the function
+module.exports = submitData;
